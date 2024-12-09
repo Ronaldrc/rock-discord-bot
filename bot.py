dcc7c761c0cf53087575aad6f7bf691d8af068db
Ron Chim <ronrchim@gmail.com>
2024-12-09 15:14:34 -0500

    Edit now-online streamer list every minute

--- bot.py
+++ bot.py
@@ -26,12 +26,9 @@ DISCORD_APPLICATION_ID = os.environ.get("DISCORD_APPLICATION_ID")
 DISCORD_PUBLIC_KEY = os.environ.get("DISCORD_PUBLIC_KEY")
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 DISCORD_LIVE_CHANNEL_ID = os.environ.get("DISCORD_LIVE_CHANNEL_ID")
+DISCORD_STREAMERS_CHANNEL_ID = os.environ.get("DISCORD_STREAMERS_CHANNEL_ID")
+DISCORD_STREAMERS_MESSAGE_ID = os.environ.get("DISCORD_STREAMERS_MESSAGE_ID")
 
-# FIXME: replace with production variables
-DISCORD_STREAM_TEXT_CHANNEL_ID = os.environ.get("DISCORD_STREAM_TEXT_CHANNEL_ID")
-DISCORD_STREAM_TEXT_MESSAGE_ID = os.environ.get("DISCORD_STREAM_TEXT_MESSAGE_ID")
-
-# FIXME: uncomment code inside on_ready function
 @client.event
 async def on_ready():
     logger.info(f"Bot is ready and logged in as {client.user}") 
@@ -65,17 +62,17 @@ async def check_twitch_streams_periodically():
     except Exception as e:
         logger.error(f"Error with Twitch streams periodic check: {e}")
 
-# @tasks.loop(seconds=60) # every minute
-# async def edit_streamer_live_not_live_msg():
-#     try:
-#         logger.info("Editing streamer live, not live msg")
-#         channel = client.get_channel(int(DISCORD_STREAM_TEXT_CHANNEL_ID))
-#         message_to_edit = await channel.fetch_message(int(DISCORD_STREAMERS_MESSAGE_ID))
-#         live, not_live = await get_live_and_offline_streamers_db()
-#         formatted_string = format_live_and_not_live_lists(live, not_live, datetime.now())
-#         await message_to_edit.edit(content=formatted_string, suppress=True)
-#     except Exception as e:
-#         logger.error(f"Failed to edit streamer live not live msg: {e}")
+@tasks.loop(seconds=60) # every minute
+async def edit_streamer_live_not_live_msg():
+    try:
+        logger.info("Editing streamer live, not live msg")
+        channel = client.get_channel(int(DISCORD_STREAMERS_CHANNEL_ID))
+        message_to_edit = await channel.fetch_message(int(DISCORD_STREAMERS_MESSAGE_ID))
+        live, not_live = await get_live_and_offline_streamers_db()
+        formatted_string = format_live_and_not_live_lists(live, not_live, datetime.now())
+        await message_to_edit.edit(content=formatted_string, suppress=True)
+    except Exception as e:
+        logger.error(f"Failed to edit streamer live, not live msg: {e}")
 
 async def main():
     await client.start(DISCORD_TOKEN)
